d2d31e3a9c170350ed019a74a80d7b1283513697
lidaw <lidaw@HP7X32PC-2>
2016-12-05 10:23:46 +0000

    Add project files.

---
+++ ITSRWD/Models/Partner.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+
+namespace ITSRWD.Models
+{
+    public class Partner
+    {
+        public int PartnerID { get; set; }
+        public string PName { get; set; }
+        public byte[] PLogo { get; set; }
+        [DataType(DataType.MultilineText)]
+        public string PInfo { get; set; }
+    }
+}
